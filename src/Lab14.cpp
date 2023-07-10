8debd1e5ef32e876be335b4b1963284aaedfc4ed
Fenton Pratt <fadpratt@gmail.com>
2023-07-09 20:59:00 -0500

    Lab14 final commit

---
+++ src/Lab14.cpp
@@ -0,0 +1,66 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/admin/Documents/CTD_2023/Labs/Lab14/src/Lab14.ino"
+#include "oled-wing-adafruit.h"
+
+void setup();
+void loop();
+void changeInterruptVal();
+#line 3 "/Users/admin/Documents/CTD_2023/Labs/Lab14/src/Lab14.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+#define btn1 D6
+#define btn2 D7
+
+OledWingAdafruit display;
+
+void isrIncrementCounter();
+
+volatile int counter = 0;
+
+bool interruptsBool = true;
+bool btnPressed = false;
+
+void setup() {
+  display.setup();
+	display.clearDisplay();
+	display.display();
+  display.setTextSize(1);
+	display.setTextColor(WHITE);
+
+  pinMode(btn1, INPUT_PULLUP);
+  pinMode(btn2, INPUT_PULLUP);
+
+  attachInterrupt(btn1, isrIncrementCounter, FALLING);
+}
+void loop() {
+  display.loop();
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(counter);
+  if(!digitalRead(btn2) && !btnPressed) {
+    interruptsBool = !interruptsBool;
+    changeInterruptVal();
+    btnPressed = true;
+  } 
+  if(digitalRead(btn2)) {
+    btnPressed = false;
+  }
+  display.display();
+}
+
+void isrIncrementCounter() {
+  counter++;
+}
+
+void changeInterruptVal() {
+  if(interruptsBool) {
+    interrupts();
+  } else {
+    noInterrupts();
+  }
+}
